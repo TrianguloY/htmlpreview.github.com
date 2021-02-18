17ebdefebe77c27826c6c56a1a70ab2cd7329fc4
TrianguloY <correo--correo@hotmail.com>
2021-02-18 22:21:42 +0100

    Tweak proxies

--- htmlpreview.js
+++ htmlpreview.js
@@ -90,9 +90,9 @@
 	var fetchProxy = function (url, options, i) {
 		var proxy = [
 			'', // try without proxy first
-			'https://cors-anywhere.herokuapp.com/',
-			'https://yacdn.org/proxy/',
-			'https://api.codetabs.com/v1/proxy/?quest='
+			// 'https://cors-anywhere.herokuapp.com/', // not available anymore
+			'https://api.codetabs.com/v1/proxy/?quest=',
+			'https://yacdn.org/proxy/', // usually timeouts
 		];
 		return fetch(proxy[i] + url, options).then(function (res) {
 			if (!res.ok) throw new Error('Cannot load ' + url + ': ' + res.status + ' ' + res.statusText);
